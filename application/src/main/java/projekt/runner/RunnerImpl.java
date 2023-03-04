8d333357a0400e60a029ee3f45b7a295f6ec3cef
Per <atlasfreak@gmx.de>
2023-03-04 01:08:28 +0100

    implement H10.2

--- application/src/main/java/projekt/runner/RunnerImpl.java
+++ application/src/main/java/projekt/runner/RunnerImpl.java
@@ -2,6 +2,7 @@ package projekt.runner;
 
 import projekt.delivery.archetype.ProblemArchetype;
 import projekt.delivery.archetype.ProblemGroup;
+import projekt.delivery.rating.RatingCriteria;
 import projekt.delivery.service.DeliveryService;
 import projekt.delivery.simulation.BasicDeliverySimulation;
 import projekt.delivery.simulation.Simulation;
@@ -10,22 +11,54 @@ import projekt.runner.handler.ResultHandler;
 import projekt.runner.handler.SimulationFinishedHandler;
 import projekt.runner.handler.SimulationSetupHandler;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-
-import static org.tudalgo.algoutils.student.Student.crash;
 
 public class RunnerImpl implements Runner {
 
     @Override
     public void run(ProblemGroup problemGroup,
-                    SimulationConfig simulationConfig,
-                    int simulationRuns,
-                    DeliveryService.Factory deliveryServiceFactory,
-                    SimulationSetupHandler simulationSetupHandler,
-                    SimulationFinishedHandler simulationFinishedHandler,
-                    ResultHandler resultHandler) {
+            SimulationConfig simulationConfig,
+            int simulationRuns,
+            DeliveryService.Factory deliveryServiceFactory,
+            SimulationSetupHandler simulationSetupHandler,
+            SimulationFinishedHandler simulationFinishedHandler,
+            ResultHandler resultHandler) {
+        Map<ProblemArchetype, Simulation> problemToSimulation = createSimulations(problemGroup, simulationConfig,
+                deliveryServiceFactory);
+        Map<RatingCriteria, List<Double>> ratingCriteriaToListOfRatings = new HashMap<>();
 
-        crash(); // TODO: H10.2 - remove if implemented
+        for (ProblemArchetype problem : problemToSimulation.keySet()) {
+            Simulation simulation = problemToSimulation.get(problem);
+
+            for (int i = 0; i < simulationRuns; i++) {
+                simulationSetupHandler.accept(simulation, problem, i);
+                simulation.runSimulation(problem.simulationLength());
+
+                if (simulationFinishedHandler.accept(simulation, problem)) {
+                    return;
+                }
+
+                for (RatingCriteria ratingCriteria : problem.raterFactoryMap().keySet()) {
+                    if (!ratingCriteriaToListOfRatings.containsKey(ratingCriteria)) {
+                        ratingCriteriaToListOfRatings.put(ratingCriteria, new ArrayList<>());
+                    }
+
+                    ratingCriteriaToListOfRatings.get(ratingCriteria)
+                            .add(simulation.getRatingForCriterion(ratingCriteria));
+                }
+            }
+        }
+
+        Map<RatingCriteria, Double> ratingCriteriaToAverageRatings = new HashMap<>();
+        ratingCriteriaToListOfRatings.forEach((ratingCriteria, ratings) -> {
+            ratingCriteriaToAverageRatings.put(ratingCriteria,
+                    ratings.stream().mapToDouble(Double::doubleValue).sum() / (double) ratings.size());
+        });
+
+        resultHandler.accept(ratingCriteriaToAverageRatings);
     }
 
     @Override
