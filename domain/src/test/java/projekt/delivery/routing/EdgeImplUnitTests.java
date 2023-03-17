f3961ac46326bebac8adec10e733184c65784a9c
Per <atlasfreak@gmx.de>
2023-03-17 02:24:41 +0100

    added H12.5

--- domain/src/test/java/projekt/delivery/routing/EdgeImplUnitTests.java
+++ domain/src/test/java/projekt/delivery/routing/EdgeImplUnitTests.java
@@ -4,8 +4,11 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import projekt.ComparableUnitTests;
 import projekt.ObjectUnitTests;
+import projekt.base.Location;
 
-import static org.tudalgo.algoutils.student.Student.crash;
+import java.util.Set;
+import java.util.function.Function;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class EdgeImplUnitTests {
@@ -22,41 +25,81 @@ public class EdgeImplUnitTests {
 
     @BeforeAll
     public static void initialize() {
-        crash(); // TODO: H12.5 - remove if implemented
+        RegionImpl region1 = new RegionImpl();
+
+        Function<Integer, Region.Edge> testObjectFactory = (i) -> {
+            RegionImpl region2 = new RegionImpl();
+            Location locationA = new Location(i - 1, i * i);
+            Location locationB = new Location(i, i * i);
+            NodeImpl nodeA = new NodeImpl(region2, "A", locationA, Set.of(locationB));
+            NodeImpl nodeB = new NodeImpl(region2, "B", locationB, Set.of(locationA));
+            region2.putNode(nodeA);
+            region2.putNode(nodeB);
+            return new EdgeImpl(region2, "Test", locationA, locationB, 0);
+        };
+        Function<Region.Edge, String> testStringFactory = (o) -> o.toString();
+        objectUnitTests = new ObjectUnitTests<>(testObjectFactory, testStringFactory);
+        objectUnitTests.initialize(10);
+        comparableUnitTests = new ComparableUnitTests<>(testObjectFactory);
+        comparableUnitTests.initialize(10);
+
+        Location locationA = new Location(0, 0);
+        Location locationB = new Location(0, 1);
+        Location locationC = new Location(1, 0);
+
+        edgeAA = new EdgeImpl(region1, "AA", locationA, locationA, 0);
+        edgeAB = new EdgeImpl(region1, "AB", locationA, locationB, 0);
+        edgeBC = new EdgeImpl(region1, "BC", locationB, locationC, 0);
+
+        nodeA = new NodeImpl(region1, "A", locationA, Set.of(locationA, locationB));
+        nodeB = new NodeImpl(region1, "B", locationB, Set.of(locationA, locationC));
+        nodeC = new NodeImpl(region1, "C", locationC, Set.of(locationB));
+
+        region1.putNode(nodeA);
+        region1.putNode(nodeB);
+        region1.putNode(nodeC);
+        region1.putEdge(edgeAA);
+        region1.putEdge(edgeAB);
+        region1.putEdge(edgeBC);
     }
 
     @Test
     public void testEquals() {
-        crash(); // TODO: H12.5 - remove if implemented
+        objectUnitTests.testEquals();
     }
 
     @Test
     public void testHashCode() {
-        crash(); // TODO: H12.5 - remove if implemented
+        objectUnitTests.testHashCode();
     }
 
     @Test
     public void testToString() {
-        crash(); // TODO: H12.5 - remove if implemented
+        objectUnitTests.testToString();
     }
 
     @Test
     public void testBiggerThen() {
-        crash(); // TODO: H12.5 - remove if implemented
+        comparableUnitTests.testBiggerThen();
     }
 
     @Test
     public void testAsBigAs() {
-        crash(); // TODO: H12.5 - remove if implemented
+        comparableUnitTests.testAsBigAs();
     }
 
     @Test
     public void testLessThen() {
-        crash(); // TODO: H12.5 - remove if implemented
+        comparableUnitTests.testLessThen();
     }
 
     @Test
     public void testGetNode() {
-        crash(); // TODO: H12.5 - remove if implemented
+        assertEquals(nodeA, edgeAA.getNodeA());
+        assertEquals(nodeA, edgeAB.getNodeA());
+        assertEquals(nodeB, edgeBC.getNodeA());
+        assertEquals(nodeA, edgeAA.getNodeB());
+        assertEquals(nodeB, edgeAB.getNodeB());
+        assertEquals(nodeC, edgeBC.getNodeB());
     }
 }
